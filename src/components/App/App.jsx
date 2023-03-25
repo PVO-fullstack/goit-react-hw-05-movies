e42ed58c9c8434e4cd268c75d67c5f4ba2b91d4f
Viacheslav Plevako <cheryec@ukr.net>
2023-03-25 10:04:20 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,7 +12,7 @@ export const App = () => {
   return (
     <div className={css.conteiner}>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route path="goit-react-hw-05-movies/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MoviesDetails />}>
